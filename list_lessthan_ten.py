ecfc5f913346cb28fc3137346f266fd277d9efdd
Elizandtech <46058821+Elizandtech@users.noreply.github.com>
2019-01-09 15:13:34 -0800

    Create list_lessthan_ten.py
    practicepython.org problem #3.
    
    Print a list with elements less than 5 from a starting list.
    Add code for third problem (extra)

---
+++ list_lessthan_ten.py
@@ -0,0 +1,13 @@
+# 1) Take a list and write a program that prints out all the elements of the list that are less than 5.
+# 2) Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+
+# 4) Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+list1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+# 3) Write this in one line of Python. - list comprehension
+print([element for element in list1 if element <5])
+
+
+# Print elements one by one:
+
