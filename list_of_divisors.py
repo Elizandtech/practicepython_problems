c08ac831974345453e8756237c4e21b60dc71a1c
Elizandtech <46058821+Elizandtech@users.noreply.github.com>
2019-01-10 11:38:53 -0800

    Change of name
    Change name of file

--- list_of_divisors.py
+++ list_of_divisors.py
@@ -1,1 +1,7 @@
-# Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+## Ask the user for a number, print out a list of all the divisors of that number.
+
+def return_divisors_of_a_number(N):
+    return [integer for integer in range(1, N+1) if N % integer == 0]
+inp_num = int(input("Enter a positive integer: "))
+print (return_divisors_of_a_number(inp_num))
+
