6ea3ae2f1898bc8c846825262547cb2646bb05d1
Elizandtech <46058821+Elizandtech@users.noreply.github.com>
2019-02-02 19:56:48 -0800

    Add prettify()
    Add parsed_html.prettify() to code.

--- decode_webpage.py
+++ decode_webpage.py
@@ -14,14 +14,16 @@ r = requests.get(url)
 # convert content to something we can read/parse:
 string_html = r.text
 
+# BeautifulSoup object with nested data structure
 parsed_html = BeautifulSoup(string_html,'html.parser')
+print(parsed_html.prettify())
 
 # retrieve first instanct of h1 tag:
 title = parsed_html.find('h1')
 
 # retrieve a list of all h1 tags:
 #title = parsed_html.find_all('h1')
-print(title)
+#print(title)
 
-for tag in title:
-    print(tag)
+#for tag in title:
+    #print(tag)
